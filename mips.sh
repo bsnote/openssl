cd7753b050bcf5d589900914e7176b8407ba7b9d
Andrei Sedoi <bsnote@gmail.com>
2013-03-05 19:52:44 +0200

    update mips.sh

--- mips.sh
+++ mips.sh
@@ -5,10 +5,10 @@ fi
 export AR="mips-linux-gnu-ar"
 export CC="mips-linux-gnu-gcc -EL"
 export CXX="mips-linux-gnu-g++ -EL"
-export CFLAGS="-march=mips32r2"
-export CXXFLAGS="-march=mips32r2"
 export LINK="mips-linux-gnu-g++ -EL"
-export LDFLAGS="-march=mips32r2"
+export CFLAGS="-mips32r2"
+export CXXFLAGS="-mips32r2"
+export LDFLAGS="-mips32r2"
 
 ./Configure linux-mips32 shared
 make
