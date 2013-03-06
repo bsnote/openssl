a3260cbab3d2ab1598b2fe5d0256bd4a993ca896
Andrei Sedoi <bsnote@gmail.com>
2013-03-06 19:06:40 +0200

    configure for cross compiling

--- mips.sh
+++ mips.sh
@@ -2,16 +2,6 @@ if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
 	export PATH="/usr/local/mips-2012.09/bin:$PATH"
 fi
 
-export AR="mips-linux-gnu-ar"
-export CC="mips-linux-gnu-gcc -EL"
-export CXX="mips-linux-gnu-g++ -EL"
-export LINK="mips-linux-gnu-g++ -EL"
-export NM="mips-linux-gnu-nm"
-export RANLIB="mips-linux-gnu-ranlib"
-export CFLAGS="-mips32r2"
-export CXXFLAGS="-mips32r2"
-export LDFLAGS="-mips32r2"
-
-./Configure linux-mips32 shared
+CROSS_COMPILE=mips-linux-gnu- ./Configure linux-mipsel32 shared
 make
 
