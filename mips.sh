9fb04ada7a2ab59f9406f4121df6a0777beec30a
Andrei Sedoi <bsnote@gmail.com>
2013-03-06 20:19:47 +0200

    enable zlib-dynamic

--- mips.sh
+++ mips.sh
@@ -2,6 +2,6 @@ if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
 	export PATH="/usr/local/mips-2012.09/bin:$PATH"
 fi
 
-CROSS_COMPILE=mips-linux-gnu- ./Configure linux-mipsel32 shared
+CROSS_COMPILE=mips-linux-gnu- ./Configure linux-mipsel32 shared zlib-dynamic
 make
 
