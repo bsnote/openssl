bf9a382b8cf1adb83b017f70cfcd14256b2a3553
Andrei Sedoi <bsnote@gmail.com>
2013-03-07 01:04:17 +0200

    minor update mips.sh

--- mips.sh
+++ mips.sh
@@ -2,7 +2,7 @@ if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
 	export PATH="/usr/local/mips-2012.09/bin:$PATH"
 fi
 
-CROSS_COMPILE=mips-linux-gnu- ./Configure linux-mipsel32 shared zlib-dynamic --prefix="/usr" --install_prefix="`pwd`/sysroot"
+CROSS_COMPILE=mips-linux-gnu- ./Configure zlib-dynamic --prefix="/usr" --install_prefix="`pwd`/sysroot" linux-mipsel32 shared
 make
 make install
 
