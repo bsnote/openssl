8c62f87f9f7914fac4a48f8e0e3224d4f2e7dea9
Andrei Sedoi <bsnote@gmail.com>
2013-03-07 03:06:55 +0200

    add certs

--- mips.sh
+++ mips.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
+
 if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
 	export PATH="/usr/local/mips-2012.09/bin:$PATH"
 fi
 
-CROSS_COMPILE=mips-linux-gnu- ./Configure zlib-dynamic --prefix="/usr" --install_prefix="`pwd`/sysroot" linux-mipsel32 shared
+SYSROOT_DIR="`pwd`/sysroot"
+PREFIX="/usr"
+
+CROSS_COMPILE=mips-linux-gnu- ./Configure zlib-dynamic --prefix="${PREFIX}" --install_prefix="${SYSROOT_DIR}" linux-mipsel32 shared
 make
 make install
+
+cd cacerts
+curl http://mxr.mozilla.org/mozilla/source/security/nss/lib/ckfw/builtins/certdata.txt?raw=1 -L -o certdata.txt
+./make-ca.sh
+./remove-expired-certs.sh certs
+cp certs/*.pem ${SYSROOT_DIR}${PREFIX}/ssl/certs
+${SYSROOT_DIR}${PREFIX}/bin/./c_rehash ${SYSROOT_DIR}${PREFIX}/ssl/certs
+install BLFS-ca-bundle*.crt ${SYSROOT_DIR}${PREFIX}/ssl/certs/ca-bundle.crt
+rm -rf certs BLFS-ca-bundle* certdata.txt
 
